f736e9855b2bd7a375d0e4b6d6c7a4afbcc8160a
Edward Edberg <edward.edberg.halim@gmail.com>
2022-05-14 10:52:08 -0700

    add deployment

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,14 +5,23 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   axios.post("http://localhost:4000/events", event); // post
   axios.post("http://localhost:4001/events", event); // comment
   axios.post("http://localhost:4002/events", event); // query
+  axios.post("http://localhost:4003/events", event); // moderation
 
   res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
